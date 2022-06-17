9746337f450d007ea25df062659b7d569a70e715
Sagnik Sen <73706036+sagniksen31@users.noreply.github.com>
2022-06-18 01:23:32 +0530

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "65.0.246.144:51641/contact.php",
+                url: "https://formspree.io/f/mayvzwnr",
                 type: "POST",
                 data: {
                     name: name,
@@ -35,11 +35,11 @@ $(function () {
                     $('#contactForm').trigger("reset");
                 },
                 error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
+                    $('#success > .alert-success').append($("<strong>").text("Thank You " + name + ", Your message has been sent."));
+                    $('#success > .alert-success').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
                 complete: function () {
